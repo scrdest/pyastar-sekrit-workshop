0996a4cfc9e0f7bea91e218d5f1e66ec441646ea
jmalek <jan.malek@gmail.com>
2022-04-04 21:32:06 +0100

    feature: Cacheable variant of the GOAP implementation.

---
+++ pyastar/reasoning/reasoning_cacheable.py
@@ -0,0 +1,158 @@
+import os
+import typing
+
+from pyastar.maputils import ActionGraph
+from pyastar.impls.goap_cachable import astar_solver
+from pyastar.reasoning.utils import State
+from pyastar.reasoning.maps import load_map_json
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+MAPDIR_SUFF = "maps"
+MAPS_DIR = os.path.join(CURR_DIR, MAPDIR_SUFF)
+
+
+def custom_map():
+    actionmap = load_map_json("map1")
+    return actionmap
+
+
+def check_preconditions_for_graph(mapobj: dict):
+
+    def _checker(neighbor, blackboard):
+
+        result = True
+        cost, preconds, effects = mapobj[neighbor]
+
+        for prec_key, prec_value in preconds.items():
+            if blackboard.get(prec_key, 0) < prec_value:
+                return False
+
+        return result
+
+    return _checker
+
+
+def get_actions(mapobj):
+
+    def _actiongetter(*args, **kwargs):
+        result = tuple(mapobj.keys())
+        return result
+
+    return _actiongetter
+
+
+def get_effects(mapobj):
+
+    def _actiongetter(action, *args, **kwargs):
+        if isinstance(action, State):
+            effects = action
+        else:
+            cost, preconds, effects = mapobj.get(action) or (float("inf"), State(), State())
+        return effects
+
+    return _actiongetter
+
+
+def get_preconds(mapobj):
+
+    def _actiongetter(action, *args, **kwargs):
+        cost, preconds, effects = mapobj[action]
+        return preconds
+
+    return _actiongetter
+
+
+def neighbor_measure(mapobj):
+
+    def _measurer(start, end):
+        cost, preconds, effects = mapobj[end]
+        return cost
+
+    return _measurer
+
+
+def goal_checker_for(mapobj):
+    effect_getter = get_effects(mapobj=mapobj)
+
+    def _goalchecker(pos, goal):
+        match = True
+        pos_effects = effect_getter(pos) if isinstance(pos, str) else pos
+
+        for state, value in goal.items():
+            if value is None:
+                continue
+
+            if pos_effects.get(state, 0) < value:
+                match = False
+                break
+
+            match = True
+
+        return match
+
+    return _goalchecker
+
+
+def preconds_checker_for(mapobj) -> typing.Callable[[typing.Union[str, dict, State], dict], bool]:
+    preconds_checker = get_preconds(mapobj=mapobj)
+
+    def _checker(action, blackboard):
+        match = True
+
+        act_preconds = preconds_checker(action) if isinstance(action, str) else action
+
+        for state, value in act_preconds.items():
+            if blackboard.get(state, 0) < value:
+                match = False
+                break
+
+        return match
+
+    return _checker
+
+
+def no_goal_heuristic(start, end):
+    return 1
+
+
+def main():
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    # goal = State.fromdict({"Fed": 1}, name="END")
+    # goal = State.fromdict({"Rested": 10}, name="END")
+    # goal = State.fromdict({"Rested": 10, "Debug": 1}, name="END")
+    # goal = State.fromdict({"Rested": 10}, name="END")
+    # goal = State.fromdict({"Rested": 10, "Debug": 1}, name="END")
+    goal = State.fromdict({"Debug": 1}, name="END")
+
+    raw_map = custom_map()
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=500,
+        max_heap_size=100,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print(cost)
+
+    newmap.visualize()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/impls/reasoning/cacheable_quick_tests.py
@@ -0,0 +1,651 @@
+import pytest
+from pyastar.reasoning.reasoning_cacheable import *
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_only", 10, 10),
+    ("debug_complex", 10, 50),
+    ("debug_complex", 10, None),
+    ("debug_complex", None, 50),
+    ("debug_complex", None, None),
+))
+def test_debug_startisend(mapname, maxiters, maxheap):
+    start = State.fromdict({"Debug": 1}, name="START")
+    goal = State.fromdict({"Debug": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert len(path) == 1
+    assert path[0] == start
+
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_only", 10, 20),
+    ("debug_only", None, 20),
+    ("debug_only", 10, None),
+    ("debug_only", None, None),
+))
+def test_debug_found_simple(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Debug": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert len(path) == 2
+    assert path[0] == start
+    assert path[1] == "DebugGetSimple"
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 1000, 500),
+))
+def test_debug_found_problematic(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Debug": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert len(path) == 2
+    assert path[0] == start
+    assert path[1] == "DebugGetSimple"
+
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("fed_only", 100, 200),
+))
+def test_fed_found_simple(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Fed": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert len(path) == 3
+    assert path[0] == start
+    assert path[1] == "GetFood"
+    assert path[2] == "Eat"
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 200, 200),
+    ("complex_nodebug", 100, 200),
+))
+def test_fed_found_complex(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert len(path) == 6
+    assert path[0] == start
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 10, 200),
+    ("complex_nodebug", 10, 200),
+))
+def test_smol_rested_found_complex(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    goal = State.fromdict({"Rested": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 400, 200),
+    ("complex_nodebug", 300, 200),
+))
+def test_big_rested_found_complex(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    goal = State.fromdict({"Rested": 10}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 400, 200),
+))
+def test_multigoal_found_complex_food_dirty(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1, "Debug": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 200, 200),
+))
+def test_multigoal_found_complex_food_clean(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasCleanDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1, "Debug": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 1000, 200),
+    ("complex_nodebug", 1000, 200),
+))
+def test_multigoal_found_complex_foodmoney_clean(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasCleanDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1, "Money": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 500, 200),
+    ("complex_nodebug", 300, 200),
+    ("complex_sleepless", 300, 200),
+))
+def test_multigoal_found_complex_foodmoney_clean(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasDirtyDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1, "Money": 1}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_only", 10, 5000),
+    ("debug_complex", 300, 5000),
+))
+def test_repeated_goal_debug(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Debug": 5}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("complex_sleepless", 200, 200),
+    ("debug_complex", 600, 450),
+    ("complex_nodebug", 200, 200),
+    ("complex_sleepless_workhard", 1500, 1000),
+))
+def test_repeated_goal_money(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Money": 50}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    import functools
+
+    cached_solver = functools.lru_cache(maxsize=10)(solve_astar)
+
+    cost, path = cached_solver(
+        start_pos=start,
+        goal=goal,
+    )
+
+    cost2, path2 = cached_solver(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
+    assert path2
+    assert path2 is path  # proof caching worked
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("complex_sleepless", 500, 500),
+    ("complex_nodebug", 700, 500),
+    # ("complex_sleepless_workhard", 4500, 2000),  # fails - cycle, I think
+))
+def test_repeated_multigoal_moneyrest(mapname, maxiters, maxheap):
+    start = State.fromdict({}, name="START")
+    goal = State.fromdict({"Money": 30, "Rested": 5}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+
+    assert path
+
+
+@pytest.mark.parametrize(("mapname", "maxiters", "maxheap"), (
+    ("debug_complex", 600, 5000),
+    ("complex_nodebug_workhard", 300, 5000),
+))
+def test_repeated_multigoal_foodrestmoney(mapname, maxiters, maxheap):
+    start = State.fromdict({"HasCleanDishes": 1}, name="START")
+    goal = State.fromdict({"Fed": 1, "Rested": 10, "Money": 10}, name="END")
+
+    raw_map = load_map_json(mapname)
+
+    newmap = (
+        ActionGraph(raw_map)
+        .set_current(start)
+        .set_goal(goal)
+    )
+
+    solve_astar = astar_solver(
+        adjacency_gen=get_actions(raw_map),
+        preconditions_check=preconds_checker_for(raw_map),
+        handle_backtrack_node=newmap.add_to_path,
+        neighbor_measure=neighbor_measure(raw_map),
+        goal_measure=no_goal_heuristic,
+        goal_check=goal_checker_for(raw_map),
+        get_effects=get_effects(raw_map),
+        cutoff_iter=maxiters,
+        max_heap_size=maxheap,
+    )
+
+    cost, path = solve_astar(
+        start_pos=start,
+        goal=goal,
+    )
+
+    print("")
+    print("COST:", cost)
+
+    newmap.visualize()
+
+    assert path
